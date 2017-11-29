7eb683f63dcf693678c3608f56e8b6d4bb0ecf6e
LouisKisaragi <motoko609@gmail.com>
2017-11-29 15:21:58 +0900

    test
    test

--- adminpage/adminpage/src/bb/admin/AdminLoginServlet.java
+++ adminpage/adminpage/src/bb/admin/AdminLoginServlet.java
@@ -10,31 +10,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bb.dao.AdminDAO;
+import bb.dao.GuestDAO;
+import bb.dto.GuestDTO;
+
 /**
  * Servlet implementation class AdminLoginServlet
+ * @param <AdminDAO>
  */
 @WebServlet("/adminLogin.do")
-public class AdminLoginServlet extends HttpServlet {
+public class AdminLoginServlet<AdminDAO> extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
     	   String id = request.getParameter("id");
     	   String pass = request.getParameter("pass");
     	   
     	   AdminDAO dao = AdminDAO.getinstance();
-    	   GuestDAO g_dao = GuestDAO.getinstance();
+   		   GuestDAO g_dao = GuestDAO.getinstance();
     	   int result = dao.checkLogin(id, pass);
     	   String url = null;
     	   HttpSession session = request.getSession();
     	   
     	   if(result > 0) 
     	   {
-    		   url = "bbADMIN/adminMain.jsp";//어드민 주소, 경로 수정해도 되지만 경로를 고칠 경우 전부 다 바꿔야 합니다.
+    		   url = "bbADMIN/adminMain.jsp";//�뼱�뱶誘� 二쇱냼, 寃쎈줈 �닔�젙�빐�룄 �릺吏�留� 寃쎈줈瑜� 怨좎튌 寃쎌슦 �쟾遺� �떎 諛붽퓭�빞 �빀�땲�떎.
     		   session.setAttribute("Admin", g_dao.successLogin(id));
     	   }
     	   else
     	   {
     		   url = "bbADMIN/adminLogin.jsp";
-    		   request.setAttribute("message","login error!");
+    		   request.setAttribute("message","login fail!");
     	   }
     	   RequestDispatcher rdp = request.getRequestDispatcher(url);
     	   rdp.forward(request, response);
