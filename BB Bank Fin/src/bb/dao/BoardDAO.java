ac388c8673487c58a573bd2207c61ed4e8f02849
LouisKisaragi <motoko609@gmail.com>
2017-12-05 10:27:55 +0900

    test
    test

---
+++ BB Bank Fin/src/bb/dao/BoardDAO.java
@@ -0,0 +1,257 @@
+package bb.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+//import bb.dto.BoardDTO;
+
+public class BoardDAO
+{
+	private BoardDAO(){}
+	private static BoardDAO instance = new BoardDAO();
+	public static BoardDAO getinstance()
+	{
+		return instance;
+	}
+	
+	public Connection getConnection() throws Exception
+	{
+		Connection conn = null;
+		Context initContext = new InitialContext();
+		Context envContext = (Context)initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource)envContext.lookup("jdbc/dbcp");
+		conn = ds.getConnection();
+		
+		return conn;
+	}
+	
+	/////////////////////////////////// 글쓰기 /////////////////////////////////////
+	public int writeBoard(BoardDTO boardDTO)
+	{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+	
+		String sql = "insert into board values(numb1_seq.nextval,?,?,?,?,default)";
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, boardDTO.getId());
+			pstmt.setString(2, boardDTO.getTitle());
+			pstmt.setString(3, boardDTO.getContents());
+			pstmt.setString(4, boardDTO.getImage());
+			result = pstmt.executeUpdate();
+
+		}catch(Exception e)	{
+			e.printStackTrace();
+		} finally {
+			close(null, pstmt, conn);
+		}
+		
+		return result;
+	}
+	
+/////////////////////////////////// 글 죄다빼오기 /////////////////////////////////////
+	public ArrayList<BoardDTO> getAllcontent(String message)
+	{
+		ArrayList<BoardDTO> list = new ArrayList<BoardDTO>();
+		String sql = null;
+		if(message.equals("all"))
+		{
+			sql = "select * from board  order by num desc";
+		}else if(message.equals("nodel"))
+		{
+			sql = "select * from board where visiable = 1 order by num desc";
+		}
+		 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardDTO dto = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				dto = new BoardDTO(rs.getInt("num"), rs.getString("guest_id"),
+						rs.getString("title"), rs.getString("contents"),
+						rs.getString("image"), rs.getInt("visiable"));
+				list.add(dto);
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		
+		return list;
+	}
+	
+	public ArrayList<QnADTO> getAllQnA() {
+		ArrayList<QnADTO> list = new ArrayList<QnADTO>();
+		String sql = "select * from board2 order by num desc";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		QnADTO dto = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				dto = new QnADTO(rs.getInt("num"), rs.getString("guest_id"),
+								rs.getString("title"), rs.getString("contents"), rs.getString("answer"));
+				list.add(dto);
+			}
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rs, pstmt, conn);
+		}
+		return list;
+	}
+	
+
+	
+	
+	
+	
+/////////////////////////////////// 글삭제&복구 /////////////////////////////////////
+	public void deleteBoard(String num, int visiable)
+	{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		String sql = "update board set visiable=? where num=?";
+		
+		try
+		{
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, visiable);
+			pstmt.setString(2, num);
+			pstmt.executeUpdate();
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}finally
+		{
+			close(null, pstmt, conn);
+		}
+	}
+	 //////////////////////////////////// 글수정 ////////////////////////////////
+	public void modifyBoard(BoardDTO boardDTO)
+	  {
+	    Connection conn = null;
+	    PreparedStatement pstmt = null;
+
+	    String sql = "update board set title=?, contents=?, image=? where num=? ";
+
+	    try
+	    {
+	      conn = getConnection();
+	      pstmt = conn.prepareStatement(sql);
+	      pstmt.setString(1, boardDTO.getTitle());
+	      pstmt.setString(2, boardDTO.getContents());
+	      pstmt.setString(3, boardDTO.getImage());
+	      pstmt.setInt(4, boardDTO.getNum());
+
+	      pstmt.executeQuery();
+
+	    } catch (Exception e)
+	    {
+	      e.printStackTrace();
+	    } finally
+	    {
+	      close(null, pstmt, conn);
+	    }
+	  }
+	
+	///////// 글 하나 가져오기 ///////
+	public BoardDTO getOneNote(int num)
+	{
+		BoardDTO dto = null;
+		Connection conn = null;
+		ResultSet rs = null;
+	    PreparedStatement pstmt = null;
+	    String sql = "select * from board where num = ?";
+	    
+	    try {
+	    	conn = getConnection();
+	    	pstmt = conn.prepareStatement(sql);
+	    	pstmt.setInt(1, num);
+	    	rs = pstmt.executeQuery();
+	    	
+	    	if(rs.next())
+	    	{
+	    		dto = new BoardDTO(rs.getInt("num"), rs.getString("guest_id"),
+	    				rs.getString("title"), rs.getString("contents"),
+	    				rs.getString("image"), rs.getInt("visiable"));
+	    	}
+	    	
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    } finally {
+	    	close(rs, pstmt, conn);
+	    }
+
+		return dto;
+	}
+	
+	//////////////////////////////////// 닫기 ////////////////////////////////
+	public void close(ResultSet rs, PreparedStatement pstmt, Connection conn)
+	{
+		if(rs!=null) // ResultSet �ݱ�
+		{
+			try 
+			{
+				rs.close();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		if(pstmt!=null)  // PreparedStatement �ݱ�
+		{
+			try
+			{
+				pstmt.close();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		
+		if(conn!=null) // Connection �ݱ�
+		{
+			try 
+			{
+				conn.close();
+			} catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
---
+++ ServletBoard1/src/BoardServlet/BoardModifyServlet.java
@@ -0,0 +1,78 @@
+package BoardServlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+//import com.oreilly.servlet.MultipartRequest;
+//import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+//import dao.BoardDAO;
+//import dto.BoardDTO;
+
+@WebServlet("/modify_board")
+public class BoardModifyServlet extends HttpServlet
+{
+  private static final long serialVersionUID = 1L;
+
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+		  throws ServletException, IOException
+  {
+    doPost(request, response);
+  }
+  
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+		  throws ServletException, IOException
+  {
+	request.setCharacterEncoding("utf-8");
+	response.setContentType("text/html; charset=utf-8");
+	HttpSession session = request.getSession();
+    
+	String id = dto.getId();
+	String savePath = "/hairIMAGE/hairSELF";
+	int uploadFileSizeLimit = 5 * 1024 * 1024;
+	String encType = "utf-8";
+	ServletContext context = getServletContext();
+	String uploadFilePath = context.getRealPath(savePath);
+	String url = "./FrontController?src=board";
+    
+	try {
+		MultipartRequest multi = new MultipartRequest(request, uploadFilePath, uploadFileSizeLimit, encType, new DefaultFileRenamePolicy());
+		String fileName = multi.getFilesystemName("uploadFile");
+		String image = multi.getParameter("image");
+		if(fileName == null)
+		{
+			System.out.println("파일 업로드 실패");
+		}
+		else
+		{
+			System.out.println("파일 업로드 성공");
+			image = fileName;
+		}
+		
+		String title = multi.getParameter("title");
+		String contents = multi.getParameter("contents");
+		int visiable = Integer.parseInt(multi.getParameter("visiable"));
+		int num = Integer.parseInt(multi.getParameter("num"));
+		
+		BoardDTO board_dto = new BoardDTO(num, id, title, contents, image, visiable);
+		BoardDAO dao = BoardDAO.getinstance();
+		dao.modifyBoard(board_dto);
+				
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+
+	RequestDispatcher rdp = request.getRequestDispatcher(url);
+	rdp.forward(request, response);
+  }
+
+}
